a70fc8683624f6580f4009aaa779b21ad6966cf2
Zee <zee@example.com>
2014-10-22 11:29:26 +0500

    Initial Commit

---
+++ GuidePost/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GuidePost
+//
+//  Created by abc on 10/22/14.
+//  Copyright (c) 2014 abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GuidePost/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GuidePost
+//
+//  Created by abc on 10/22/14.
+//  Copyright (c) 2014 abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
